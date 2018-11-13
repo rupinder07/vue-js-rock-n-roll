a47d3b38c65f64af96fd666ab1af16f2157b871b
Rupinder Singh <rupinder.singh@nagarro.com>
2018-11-13 12:38:33 +0530

    Added band to band list

---
+++ src/components/BandItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <li @click="toggle">
+            {{band.name}}
+            <p v-show="open">
+                {{band.songs | join}}
+                <input type="text" placeholder="Add more songs" @click.stop @keyup.enter="addSong" v-model='newSong'/>
+            </p>
+
+
+        </li>
+    </div>    
+</template>
+
+<script>
+import BandItem from './BandItem.vue'
+
+export default {
+    name: 'BandItem',
+    data(){
+        return {
+            newSong: '',
+            open: false,
+        }
+    },
+    props: {
+        band: Object
+    },
+    filters: {
+        join(arr) {
+            return arr.join(', ')
+        }
+    },
+    methods: {
+        toggle() {
+            this.open = !this.open;
+        },
+        addSong() {
+            this.band.songs.push(this.newSong);
+        }
+    }
+}
+</script>
---
+++ src/components/BandList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <input type="text" placeholder="Add Band" v-model='bandName' @click="addBand" @keyup.enter="addBand">
+        <ul>
+            <BandItem v-for="band in bands" :key="band.name" v-bind:band="band">
+            </BandItem>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BandItem from './BandItem.vue'
+
+export default {
+    name: 'BandList',
+    data() {
+        return {
+            bandName: '',
+            bands: [
+                {
+                    id: 1,
+                    name: 'Beetles',
+                    songs: [
+                        "one", "two"
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'Cold Play',
+                    songs: [
+                        "three", "four"
+                    ]
+                    
+                }
+            ]
+        }
+    },
+    components: {
+        BandItem
+    },
+    methods: {
+        addBand(){
+            this.bands.push({id:3, name: this.bandName, songs: []})
+        }
+    }
+}
+</script>
